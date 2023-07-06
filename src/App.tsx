5eb558660d5d8b1c5e9813e030776d1ad051f0fc
Ali-consciousroad <ali_dindar@live.be>
2023-07-06 15:56:57 +0200

    Set up the server state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import PokemonList from "./Pokedex/PokemonList";
+
+const queryClient = new QueryClient();
+
 const App = () => {
-  return <></>
+  return (
+  <QueryClientProvider client={queryClient}>
+    <PokemonList />
+  </QueryClientProvider>
+  );
 };
 
 export default App;
